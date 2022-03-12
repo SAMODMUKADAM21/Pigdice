356d70b882d5033f974a6f5cae923287a344749f
samod mukadam <samodmukadam2005@gmail.com>
2022-03-11 16:39:55 -0800

    Create function to displayscore

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
 import Pigdice from './Pigdice.js';
+
+let pigDice = new PigDice();
+
+// function for dispaly the score
+function dispalyScore(){
+    $("#player-1score").text(pigDice.player1);
+    $("#player-2score").text(pigDice.player2);
+}
